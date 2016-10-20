a51d64e19e13c84ed0eba07650382282b2ed54b7
MadeshwaranV <inkcemadeshece@gmail.com>
2016-10-20 15:30:08 +0530

    Update sum of n.c

--- sum of n.c
+++ sum of n.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-int n,sum = 0,c,d; 
+int n,sum=0,c,d; 
 printf("Enter the numbers");
 scanf("%d",&n);
 printf("Enter %d integers\n",n);
-for(c = 1;c<=n;c++)
+for(c=1;c<=n;c++)
 {
-scanf("%d", &d);
+scanf("%d",&d);
 sum =sum+d;
 }
 printf("Sum of entered integers = %d\n",sum);

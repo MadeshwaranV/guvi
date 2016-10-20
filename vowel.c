bfc7e5481f5647afc51f53ff00adb82b1c8d4849
MadeshwaranV <inkcemadeshece@gmail.com>
2016-10-20 14:08:30 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+char t;
+scanf("%c",&b);
+if(t=='a'||t=='e'||t=='i'||t=='o'||t=='u')
+printf("The character is vowel");
+}
+else
+{
+printf("The vowel is CONSTANT");
+return 0;
+}
